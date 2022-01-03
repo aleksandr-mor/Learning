183e217f27879e4b1b6acf304e5247ac31f9b0a3
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-01-04 01:52:29 +0300

    UI training project

---
+++ Friend Book/Friend List/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Friend List
+//
+//  Created by Aleksandr Morozov on 03.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    let font = UIFont(name: "Charter-Bold", size: 14)!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        image.layer.masksToBounds = true
+        image.layer.cornerRadius = image.bounds.width / 2
+        image.layer.borderWidth = 2
+        image.layer.borderColor = #colorLiteral(red: 0.3783450127, green: 0.4734536409, blue: 0.9340841174, alpha: 1)
+        
+        saveButton.layer.cornerRadius = 5
+        saveButton.titleLabel?.font =  UIFont(name: "Charter-Bold", size: 16)
+
+        addButton.layer.cornerRadius = 20
+        addButton.layer.borderWidth = 1
+        addButton.layer.borderColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+//        addButton.layer.masksToBounds = false
+//        addButton.layer.cornerRadius = image.bounds.width / 2
+        
+    }
+  //  #colorLiteral(red: 0.3783450127, green: 0.4734536409, blue: 0.9340841174, alpha: 1)
+  
+}
+
