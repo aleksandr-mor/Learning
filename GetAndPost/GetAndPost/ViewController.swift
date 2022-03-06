9aaf626a611b84bd8b1efccd1640ee1b044d9fc7
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-03-07 00:08:55 +0300

    Add files via upload

--- GetAndPost/GetAndPost/ViewController.swift
+++ GetAndPost/GetAndPost/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBAction func getTapped(_ sender: UIButton) {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
@@ -18,12 +18,43 @@ class ViewController: UIViewController {
                 print(response)
             }
             
+            
             guard let data = data else { return }
-        }
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
     
     @IBAction func PostTapped(_ sender: UIButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let parameters = ["username": "Aleksandr", "message": "Hello World!"]
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            guard let data = data else { return }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
-    
 }
 
