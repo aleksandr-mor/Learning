412a45a3b49b88ee141e091702fd3aec2d92c999
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-03-31 23:03:07 +0300

    Add files via upload

---
+++ Tab Bar Controller Tutorial/Tab Bar Controller Tutorial/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Tab Bar Controller Tutorial
+//
+//  Created by Aleksandr Morozov on 31.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        // TODO: Here, perform login with tab controller
+    }
+    
+}
+
---
+++ Tab Bar Controller Tutorial/Tab Bar Controller Tutorial/MainTabController.swift
@@ -0,0 +1,11 @@
+//
+//  MainTabController.swift
+//  Tab Bar Controller Tutorial
+//
+//  Created by Aleksandr Morozov on 31.03.2022.
+//
+
+import Foundation
+import UIKit
+
+
