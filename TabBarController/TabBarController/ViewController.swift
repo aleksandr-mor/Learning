495d3d3d33409c03fdcd5dc2f695e8e0f39ebc93
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-05-12 23:49:37 +0300

    Add files via upload

---
+++ TabBarController/TabBarController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarController
+//
+//  Created by Aleksandr Morozov on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
