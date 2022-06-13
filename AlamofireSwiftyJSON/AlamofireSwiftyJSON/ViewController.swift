0ea8eb3b0208e236d664439c915eed9e5d4da332
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-06-13 23:49:06 +0300

    starting

---
+++ AlamofireSwiftyJSON/AlamofireSwiftyJSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireSwiftyJSON
+//
+//  Created by Aleksandr Morozov on 13.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
