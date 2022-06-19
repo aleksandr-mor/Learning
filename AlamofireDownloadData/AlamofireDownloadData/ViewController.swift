5fe6c3abc19b276d2068a07abbbc149f3ca61280
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-06-19 15:08:14 +0300

    test

---
+++ AlamofireDownloadData/AlamofireDownloadData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireDownloadData
+//
+//  Created by Aleksandr Morozov on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AlamofireDownloadData/Pods/Target Support Files/Pods-AlamofireDownloadData/Pods-AlamofireDownloadData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlamofireDownloadData : NSObject
+@end
+@implementation PodsDummy_Pods_AlamofireDownloadData
+@end
