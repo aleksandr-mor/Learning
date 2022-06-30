2a2090f1578160a85d4c0762f6f37ef1e5878702
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-06-30 22:59:47 +0300

    MVC

---
+++ MVC/MVC/Controller/ShopController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Aleksandr Morozov on 30.06.2022.
+//
+
+import UIKit
+
+class ShopController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVC/MVC/Model/ShopModel.swift
@@ -0,0 +1,8 @@
+//
+//  ShopModel.swift
+//  MVC
+//
+//  Created by Aleksandr Morozov on 30.06.2022.
+//
+
+import Foundation
