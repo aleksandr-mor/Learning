9bd729c3bd8b25a178b4c5066417b55b585aa58f
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-07-05 23:01:06 +0300

    MVC

---
+++ JustAnotherMVC/JustAnotherMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustAnotherMVC
+//
+//  Created by Aleksandr Morozov on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
