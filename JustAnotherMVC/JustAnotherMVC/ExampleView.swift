3e57b51430a73d070af7fee45eecd7d5047691bb
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-07-06 22:21:42 +0300

    final

--- JustAnotherMVC/JustAnotherMVC/ExampleView.swift
+++ JustAnotherMVC/JustAnotherMVC/ExampleView.swift
@@ -8,25 +8,31 @@
 import UIKit
 
 class ExampleView: UIView {
-
+    
     private let label: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
+        label.backgroundColor = .black
+        label.textColor = .white
         return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .red
+        addSubview(label)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    public func configure(with name: String) {
+        label.text = name
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         label.frame = CGRect(x: 10, y: 10, width: frame.size.width - 20, height: frame.size.height - 20)
     }
-    
 }
--- JustAnotherMVC/JustAnotherMVC/ViewController.swift
+++ JustAnotherMVC/JustAnotherMVC/ViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let person = Person(
+        firstName: "Aleks",
+        lastName: "Smith",
+        gender: "Male",
+        age: "35",
+        weight: 90.0,
+        height: 90.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
-
+        let myView = ExampleView(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        myView.center = view.center
+        myView.configure(with: "\(person.firstName) \(person.lastName)")
+        view.addSubview(myView)
+    }
 }
 
 struct Person {
@@ -24,9 +39,5 @@ struct Person {
     let age: String
     let weight: Double
     let height: Double
-    let location: Location
 }
 
-struct Location {
-    let city: String
-}
