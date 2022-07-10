ca866791bc8b66c9f46a034fce6bad3bf6f50e1a
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-07-10 23:00:35 +0300

    Update ViewController.swift

--- JustAnotherMVC/JustAnotherMVC/ViewController.swift
+++ JustAnotherMVC/JustAnotherMVC/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidLayoutSubviews() {
