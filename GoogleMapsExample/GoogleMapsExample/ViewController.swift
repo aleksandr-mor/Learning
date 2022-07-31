e4749cbe1d93ce818dcdd037b2213a627154bff2
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-07-31 21:43:36 +0300

    Starting a project

---
+++ GoogleMapsExample/GoogleMapsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoogleMapsExample
+//
+//  Created by Aleksandr Morozov on 31.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
