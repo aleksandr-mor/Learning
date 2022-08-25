e5c887e77ee629d1096b96075cfafd232bca05b0
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-08-25 21:41:31 +0300

    test

---
+++ AnotherAutoLayout/AnotherAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnotherAutoLayout
+//
+//  Created by Aleksandr Morozov on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
