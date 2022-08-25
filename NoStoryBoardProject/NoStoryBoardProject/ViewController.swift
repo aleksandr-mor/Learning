752f1b0f780729810fd4f99cb12add4b49a28d49
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-08-25 22:42:17 +0300

    testing

---
+++ NoStoryBoardProject/NoStoryBoardProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoStoryBoardProject
+//
+//  Created by Aleksandr Morozov on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
