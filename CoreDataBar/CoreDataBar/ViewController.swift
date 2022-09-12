1afedda29ec8492af1d271bcdc798f858b9283d2
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-09-12 22:52:22 +0300

    starting

---
+++ CoreDataBar/CoreDataBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataBar
+//
+//  Created by Aleksandr Morozov on 12.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let employee = Employee
+    }
+
+
+}
+
